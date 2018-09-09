90756610def4ca302dc5e345be7f80730d77117a
John Rogers <johnrogers@johns-mbp.attlocal.net>
2018-09-09 18:02:54 -0500

    Initial model commit.

---
+++ db/conn.js
@@ -0,0 +1,12 @@
+var knex = require('knex')({
+  client: 'pg',
+  version: '7.4.3',
+  connection: {
+    host: '127.0.0.1',
+    user: 'your_database_user',
+    password: 'your_database_password',
+    database: 'myapp_test'
+  }
+});
+
+export default knex;
---
+++ db/sampleData/ingredients.js
@@ -0,0 +1,106 @@
+let ingredients = [{
+    name: "Cashew",
+    price: 7.97,
+  },
+  {
+    name: "Orange",
+    price: 4.00,
+  },
+  {
+    name: "green onion",
+    price: 0.43,
+  },
+  {
+    name: "Celery",
+    price: 0.88,
+  },
+  {
+    name: "Roma Tomato",
+    price: 0.88,
+  },
+  {
+    name: "Bananna",
+    price: 0.44,
+  },
+  {
+    name: "Pepper",
+    price: 1.28,
+  },
+  {
+    name: "Strawberries",
+    price: 1.47,
+  },
+  {
+    name: "Mushroom",
+    price: 1.48,
+  },
+  {
+    name: "Sweet Potato",
+    price: 0.98,
+  },
+  {
+    name: "Onion",
+    price: 0.88,
+  },
+  {
+    name: "Apples",
+    price: 3.98,
+  },
+  {
+    name: "Topo",
+    price: 0.70,
+  },
+  {
+    name: "Spinach",
+    price: 4.35,
+  },
+  {
+    name: "Blackberries",
+    price: 2.18,
+  },
+  {
+    name: "Chicken Drumsticks",
+    price: 4.62,
+  },
+  {
+    name: "Cesar",
+    price: 4.98,
+  },
+  {
+    name: "Cherry Tomato",
+    price: 5.48,
+  },
+  {
+    name: "shampoo",
+    price: 5.74,
+  },
+  {
+    name: "Topo Bottle",
+    price: 1.19,
+  },
+  {
+    name: "Turkey",
+    price: 2.99,
+  },
+  {
+    name: "Mango",
+    price: 6.98,
+  },
+  {
+    name: "Chicken Breast",
+    price: 7.28,
+  },
+  {
+    name: "Eggs",
+    price: 2.58,
+  },
+  {
+    name: "Pinapple",
+    price: 3.88,
+  },
+  {
+    name: "Avocado",
+    price: 1.38,
+  },
+]
+export default ingredients;
