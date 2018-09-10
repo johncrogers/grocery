ed61764fd7652b8b6b6f0c0229d39296e7f4af19
John Rogers <johnrogers@johns-mbp.attlocal.net>
2018-09-10 06:56:24 -0500

    Create basic nav menu.

--- app/src/components/common/_custom/Nav/Nav.jsx
+++ app/src/components/common/_custom/Nav/Nav.jsx
@@ -4,12 +4,22 @@ class Nav extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.handleMenuChange = this.handleMenuChange.bind(this);
+  }
+  handleMenuChange(targetView) {
+    let newView = this.props.view;
+    newView.show = targetView.target.value;
+    this.props.updateView(newView);
   }
   render() {
     // console.log(`Render Nav`, this.props);
     return (
       <div id="Nav">
-        Menu&nbsp;|&nbsp;
+        <select name="Menu" id="Menu" onChange={this.handleMenuChange}>
+          <option value="Cart">Cart</option>
+          <option value="Menu">Menu</option>
+        </select>
+        &nbsp;|&nbsp;
         {this.props.view.show}
       </div>
     );
