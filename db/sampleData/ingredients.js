4d0cdb36ead89c912f508d34472b1408c0c62da7
John Rogers <johnrogers@johns-mbp.attlocal.net>
2018-09-09 22:18:22 -0500

    Add department field to sample data.

--- db/sampleData/ingredients.js
+++ db/sampleData/ingredients.js
@@ -1,106 +1,147 @@
 let ingredients = [{
     name: "Cashew",
     price: 7.97,
+    department: "Produce",
   },
   {
     name: "Orange",
     price: 4.00,
+    department: "Fruit",
   },
   {
-    name: "green onion",
+    name: "Green Onion",
     price: 0.43,
+    department: "Produce",
   },
   {
     name: "Celery",
     price: 0.88,
+    department: "Produce",
   },
   {
     name: "Roma Tomato",
     price: 0.88,
+    department: "Produce",
   },
   {
     name: "Bananna",
     price: 0.44,
+    department: "Produce",
   },
   {
-    name: "Pepper",
+    name: "Bell Peppers",
     price: 1.28,
+    department: "Produce",
   },
   {
     name: "Strawberries",
     price: 1.47,
+    department: "Fruit",
   },
   {
     name: "Mushroom",
     price: 1.48,
+    department: "Produce",
   },
   {
     name: "Sweet Potato",
     price: 0.98,
+    department: "Produce",
   },
   {
     name: "Onion",
     price: 0.88,
+    department: "Produce",
   },
   {
     name: "Apples",
     price: 3.98,
+    department: "Fruit",
   },
   {
-    name: "Topo",
+    name: "Topo (Glass)",
     price: 0.70,
+    department: "Drinks",
   },
   {
     name: "Spinach",
     price: 4.35,
+    department: "Produce",
   },
   {
     name: "Blackberries",
     price: 2.18,
+    department: "Fruit",
   },
   {
     name: "Chicken Drumsticks",
     price: 4.62,
+    department: "Meat",
   },
   {
     name: "Cesar",
     price: 4.98,
+    department: "Produce",
   },
   {
     name: "Cherry Tomato",
     price: 5.48,
+    department: "Produce",
   },
   {
-    name: "shampoo",
+    name: "Shampoo",
     price: 5.74,
+    department: "Home Goods",
   },
   {
-    name: "Topo Bottle",
+    name: "Topo (Plastic)",
     price: 1.19,
+    department: "Drinks",
   },
   {
     name: "Turkey",
     price: 2.99,
+    department: "Meat",
   },
   {
     name: "Mango",
     price: 6.98,
+    department: "Fruit",
   },
   {
     name: "Chicken Breast",
     price: 7.28,
+    department: "Meat",
   },
   {
     name: "Eggs",
     price: 2.58,
+    department: "Dairy",
   },
   {
     name: "Pinapple",
     price: 3.88,
+    department: "Fruit",
   },
   {
     name: "Avocado",
     price: 1.38,
+    department: "Produce",
   },
 ]
 export default ingredients;
+
+// fruit
+// veggie
+// bread
+// cheese
+// meat
+// wine / beer
+// baking
+// pasta
+// canned
+// spice
+// dairy
+// drinks
+// home goods
+// dessert
