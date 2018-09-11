8a933481baf836de471a9ab9338c8d3e878898bf
John Rogers <johnrogers@johns-mbp.attlocal.net>
2018-09-10 20:10:09 -0500

    Secure connection.

--- db/conn.js
+++ db/conn.js
@@ -1,12 +1,12 @@
-var conn = require('knex')({
+console.log(`Aquiring connection to database: GroceryApplication...`);
+
+module.exports.conn = require('knex')({
   client: 'pg',
   version: '7.4.3',
   connection: {
-    host: '127.0.0.1',
-    user: 'your_database_user',
-    password: 'your_database_password',
-    database: 'myapp_test'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
   }
 });
-
-export default conn;
