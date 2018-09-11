26c92b14668c01ecf3978f779c5e8e5831beea63
John Rogers <johnrogers@johns-mbp.attlocal.net>
2018-09-10 20:10:37 -0500

    Finish initial ingredients migration.

--- db/migrations/ingredients.js
+++ db/migrations/ingredients.js
@@ -1,14 +1,28 @@
-import conn from '../conn.js';
+const conn = require('../conn.js').conn;
 
-module.exports.createTable = conn.schema.createTable('users', function (table) {
-  table.increments();
-  table.string('name');
-  table.string('department');
-  table.string('note');
-  table.integer('price');
-}).then(() => {
-  console.log(`  -> Table created.`);
-});
-module.exports.dropTable = conn.schema.dropTable('users').then(() => {
-  console.log(`  -> Table dropped.`);
-});
+module.exports.createTable = () => {
+  console.log(`Running ingredients createTable schema.`);
+  return conn.schema.createTable('ingredients', function (table) {
+    table.increments();
+    table.string('name');
+    table.string('department');
+    table.string('note');
+    table.integer('price');
+  }).then(() => {
+    console.log(`Table 'ingredients' created.`);
+  }).catch((err) => {
+    console.log(`Error while creating table. ERR:`, err);
+  }).finally(() => {
+    conn.destroy();
+  });
+}
+module.exports.dropTable = () => {
+  console.log(`Running ingredients dropTable schema.`);
+  return conn.schema.dropTable('ingredients').then(() => {
+    console.log(`Table 'ingredients' dropped.`);
+  }).catch((err) => {
+    console.log(`Error while dropping table. ERR:`, err);
+  }).finally(() => {
+    conn.destroy();
+  });
+}
