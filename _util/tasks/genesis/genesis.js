b2ba3d7f453c985496b476f518cf5610e314b551
John Rogers <johnrogers@johns-mbp.attlocal.net>
2018-09-28 23:42:35 -0500

    Major genesis script.

--- _util/tasks/genesis/genesis.js
+++ _util/tasks/genesis/genesis.js
@@ -1,14 +1,35 @@
 // npm run task genesis generate tableName
 module.exports.generate = () => {
   const { execSync } = require("child_process");
-  let tableName = process.argv[4];
-  let commands = [
+  const tableName = process.argv[4];
+  console.log(`GENESIS GENERATE: ${tableName}`);
+  const commands = [
     `npm run task db generateSchema ${tableName}`,
     `npm run task db generateModel ${tableName}`,
     `npm run task api generateEndpoint ${tableName}`,
     `npm run task db create ${tableName}`
   ];
   commands.forEach(command => {
-    execSync(command);
+    execSync(command, { stdio: [0, 1, 2] }, (error, stdout, stderr) => {
+      if (error) {
+        console.log(`GENESIS ERROR:`, err);
+        console.log(stdout);
+        console.log(stderr);
+      }
+    });
+  });
+};
+module.exports.buildFromConfigTables = () => {
+  const { execSync } = require("child_process");
+  const { tables } = require("./../appConfig.js");
+  Object.keys(tables).forEach(table => {
+    const command = `npm run task genesis generate ${table}`;
+    execSync(command, { stdio: [0, 1, 2] }, (error, stdout, stderr) => {
+      if (error) {
+        console.log(`GENESIS ERROR:`, err);
+        console.log(stdout);
+        console.log(stderr);
+      }
+    });
   });
 };
